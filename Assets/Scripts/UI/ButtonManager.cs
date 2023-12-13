ba31d5fdf3c302fde2c01737d2c8fd457b66eb67
Lawrence1031h <lawrence1031h@gmail.com>
2023-12-13 10:30:22 +0900

    20231213_중간저장
    GameManager에서
    인벤토리 관련 스크립트 분리 작업 예정
    백업용

--- Assets/Scripts/UI/ButtonManager.cs
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -8,6 +8,13 @@ public class ButtonManager : MonoBehaviour
     public GameObject StatusBoard;
     public GameObject InventoryButton;
     public GameObject InventoryBoard;
+    public GameObject InventoryPrevButton;
+    public GameObject InventoryNextButton;
+    public GameObject InventorySlot1;
+    public GameObject InventorySlot2;
+    public GameObject InventorySlot3;
+    public GameObject InventorySlot4;
+    public GameObject InventorySlot5;
     public GameObject backButton;
 
     // 시작 시에 StatusBoard, InventoryBoard, backButton false로
@@ -21,6 +28,11 @@ public class ButtonManager : MonoBehaviour
         StatusBoard.SetActive(false);
         InventoryBoard.SetActive(false);
         backButton.SetActive(false);
+        InventorySlot1.SetActive(false);
+        InventorySlot2.SetActive(false);
+        InventorySlot3.SetActive(false);
+        InventorySlot4.SetActive(false);
+        InventorySlot5.SetActive(false);
     }
 
     public void ClickStatus()
@@ -44,6 +56,5 @@ public class ButtonManager : MonoBehaviour
             InventoryBoard.SetActive(false);
 
         backButton.SetActive(false);
-
     }
 }
