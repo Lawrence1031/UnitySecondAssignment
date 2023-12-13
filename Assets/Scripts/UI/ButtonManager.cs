ee75900fbac0958fb218832acb91615aa2c447f3
Lawrence1031h <lawrence1031h@gmail.com>
2023-12-13 10:48:01 +0900

    20231213_중간저장
    10:47
    우선 인벤토리 구현 후에 GameManger
    기능 분리 작업을 위한 백업용

--- Assets/Scripts/UI/ButtonManager.cs
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
@@ -16,6 +17,10 @@ public class ButtonManager : MonoBehaviour
     public GameObject InventorySlot4;
     public GameObject InventorySlot5;
     public GameObject backButton;
+
+    public TextMeshProUGUI InventoryPage;
+
+    public int CurPage = 1;
 
     // 시작 시에 StatusBoard, InventoryBoard, backButton false로
     // StatusButton을 누르면 StatusBoard, backButton true
@@ -44,6 +49,7 @@ public class ButtonManager : MonoBehaviour
     public void ClickInventory()
     {
         InventoryBoard.SetActive(true);
+        InventorySlot1.SetActive(true);
         backButton.SetActive(true);
     }
 
@@ -56,5 +62,77 @@ public class ButtonManager : MonoBehaviour
             InventoryBoard.SetActive(false);
 
         backButton.SetActive(false);
+    }
+
+    public void InvenNext()
+    {
+        // 지금은 if 문의 반복이지만,
+        // 이후에는 true인거 찾아서 false 만들고 그 다음 slot을 true로 만들기로
+        if (InventorySlot1.activeSelf)
+        {
+            InventorySlot1.SetActive(false);
+            InventorySlot2.SetActive(true);
+            CurPage++;
+        }
+        else if (InventorySlot2.activeSelf)
+        {
+            InventorySlot2.SetActive(false);
+            InventorySlot3.SetActive(true);
+            CurPage++;
+        }
+        else if (InventorySlot3.activeSelf)
+        {
+            InventorySlot3.SetActive(false);
+            InventorySlot4.SetActive(true);
+            CurPage++;
+        }
+        else if (InventorySlot4.activeSelf)
+        {
+            InventorySlot4.SetActive(false);
+            InventorySlot5.SetActive(true);
+            CurPage++;
+        }
+        else if (InventorySlot5.activeSelf)
+        {
+            InventorySlot5.SetActive(true);
+        }
+
+        InventoryPage.text = CurPage.ToString() + " / 5";
+    }
+
+    public void InvenPrev()
+    {
+        // 지금은 if 문의 반복이지만,
+        // 이후에는 true인거 찾아서 false 만들고 이전 slot을 true로 만들기로
+        if (InventorySlot1.activeSelf)
+        {
+            InventorySlot1.SetActive(true);
+        }
+        else if (InventorySlot2.activeSelf)
+        {
+            InventorySlot2.SetActive(false);
+            InventorySlot1.SetActive(true);
+            CurPage--;
+        }
+        else if (InventorySlot3.activeSelf)
+        {
+            InventorySlot3.SetActive(false);
+            InventorySlot2.SetActive(true);
+            CurPage--;
+        }
+        else if (InventorySlot4.activeSelf)
+        {
+            InventorySlot4.SetActive(false);
+            InventorySlot3.SetActive(true);
+            CurPage--;
+        }
+        else if (InventorySlot5.activeSelf)
+        {
+            InventorySlot5.SetActive(false);
+            InventorySlot4.SetActive(true);
+            CurPage--;
+        }
+
+        InventoryPage.text = CurPage.ToString() + " / 5";
     }
 }
