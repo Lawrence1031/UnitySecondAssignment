2a435d27f94d1c1004fc125121c3bb602b608ba5
Lawrence1031h <lawrence1031h@gmail.com>
2023-12-13 17:22:54 +0900

    20231213_중간저장
    오류로 종료되어 복구 후에 저장
    아이템을 두 종류(장비, 소모품)으로 나눠서 prefab을 만들 예정

--- Assets/Scripts/Item/EquipItemButton.cs
+++ Assets/Scripts/Item/EquipItemButton.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ItemButtonOld : MonoBehaviour
+public class EquipItemButton : MonoBehaviour
 {
+    //아이템 클릭 시에 장비인지 소모품인지 확인하고
+    //장비인 경우에는 장착할 수 있게,
+    //소모품인 경우에는 소모 시에 사라지게 만들기
+
     public GameObject Equip;
 
     private void Start()
